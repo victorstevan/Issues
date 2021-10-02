bb63ccb1575b61519dc5789a619362b4ccee3629
Victor Stevan <viktor.stevan1999@gmail.com>
2021-10-02 19:06:31 -0300

    chore: refactor cli module to more idiomatic elixir approach

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -21,24 +21,23 @@ defmodule Issues.CLI do
   """
 
   def parse_args(argv) do
-    parse =
-      OptionParser.parse(argv,
-        switches: [help: :boolean],
-        aliases: [h: :help]
-      )
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    |> elem(1)
+    |> args_to_internal_representation()
+  end
 
-    case parse do
-      {[help: true], _args, _errors} ->
-        :help
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
 
-      {_parsed, [user, project, count], _} ->
-        {user, project, String.to_integer(count)}
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
 
-      {_, [user, project], _} ->
-        {user, project, @default_count}
-
-      _ ->
-        :help
-    end
+  def args_to_internal_representation(_) do
+    :help
   end
 end
