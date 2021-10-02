e5ed378d4f3a00e3d760d928031e245bc3525916
Victor Stevan <viktor.stevan1999@gmail.com>
2021-10-02 20:22:57 -0300

    feat: add table formatter and its tests

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [print_table_for_columns: 2]
+
   @default_count 4
 
   @moduledoc """
@@ -50,10 +52,18 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     Issues.GithubIssues.fetch(user, project)
-    |> decode_response()
-    |> sort_into_descending_order()
+    |> decode_response
+    |> sort_into_descending_order
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 
   def sort_into_descending_order(list_of_issues) do
