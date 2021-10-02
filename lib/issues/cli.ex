5737ac424622ad985d9f47a06c899ad63b2e0a81
Victor Stevan <viktor.stevan1999@gmail.com>
2021-10-02 19:01:33 -0300

    feat: add cli parsing module and tests

---
+++ lib/issues/cli.ex
@@ -0,0 +1,44 @@
+defmodule Issues.CLI do
+  @default_count 4
+
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  `argv` can he -h or --help, which returns :help.
+
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+
+  Return a tuple of `{ user, project, count } `, or `:healp` if help was given
+  """
+
+  def parse_args(argv) do
+    parse =
+      OptionParser.parse(argv,
+        switches: [help: :boolean],
+        aliases: [h: :help]
+      )
+
+    case parse do
+      {[help: true], _args, _errors} ->
+        :help
+
+      {_parsed, [user, project, count], _} ->
+        {user, project, String.to_integer(count)}
+
+      {_, [user, project], _} ->
+        {user, project, @default_count}
+
+      _ ->
+        :help
+    end
+  end
+end
